02a8ac91ab009682e40ce8528965f64b4800c495
sujithust <147375608+sujithust@users.noreply.github.com>
2023-10-11 10:36:53 +0530

    Update hello_map.groovy

--- vars/hello_map.groovy
+++ vars/hello_map.groovy
@@ -1,3 +1,24 @@
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
+
+
+
+
+
+
+// @Library('sujith') _
+
+// pipeline {
+//     agent any
+
+//     stages {
+//         stage('Hello World') {
+//             steps {
+//                 script {
+//                     hello_map(name: 'abc', dayOfWeek: 'thu')
+//                 }
+//             }
+//         }
+//     }
+// }
