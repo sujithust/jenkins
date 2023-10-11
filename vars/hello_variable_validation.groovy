f7d070e8a9a399cc168dd3e1a0a2a628a09f1e6f
sujithust <147375608+sujithust@users.noreply.github.com>
2023-10-11 15:10:40 +0530

    removing parsing logic

--- vars/hello_variable_validation.groovy
+++ vars/hello_variable_validation.groovy
@@ -1,10 +1,15 @@
 def call(String name, int dayOfWeek) {
     // Validate the 'name' and 'dayOfWeek' parameters
-    if (isString(name) && isInteger(dayOfWeek)) {
-        // If 'name' is a non-null string and 'dayOfWeek' is an integer, execute the 'sh' step
-        sh "echo Hello World ${name}. It is day ${dayOfWeek}."
+    if (isString(name)) {
+        // 'name' is a non-null string, proceed with the validation for 'dayOfWeek'
+        if (isInteger(dayOfWeek)) {
+            // 'dayOfWeek' is an integer, execute the 'sh' step
+            sh "echo Hello World ${name}. It is day ${dayOfWeek}."
+        } else {
+            error("Invalid input: 'dayOfWeek' must be an integer.")
+        }
     } else {
-        error("Invalid input: 'name' must be a non-null string, and 'dayOfWeek' must be an integer.")
+        error("Invalid input: 'name' must be a non-null string.")
     }
 }
 
@@ -15,11 +20,7 @@ def isString(value) {
 
 // Function to check if a value is an integer
 def isInteger(value) {
-    try {
-        Integer.parseInt(value)
-        return true
-    } catch (NumberFormatException e) {
-        return false
-    }
+    return value instanceof Integer
 }
+
 
