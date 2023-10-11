c68ca061091a3b2d5314fa8b49a1f8d00c9559ee
sujithust <147375608+sujithust@users.noreply.github.com>
2023-10-11 15:48:27 +0530

    Update hello_if_else_try_catch.groovy

--- vars/hello_if_else_try_catch.groovy
+++ vars/hello_if_else_try_catch.groovy
@@ -3,12 +3,16 @@
 def call(String fileName) {
     try {
         if (isFileExists(fileName)) {
-            echio "File '$fileName' exists. Performing some action..."
+            echo "File '$fileName' exists. Performing some action..."
             // You can add more steps to perform actions when the file exists
         } else {
             echo "File '$fileName' does not exist. Displaying an error message..."
             // You can add more steps for handling the absence of the file
         }
+        
+        // Intentionally trigger an exception for testing
+        def nonExistentFile = new File('/path/to/nonexistentfile.txt')
+        nonExistentFile.text // This line will generate an exception
     } catch (Exception e) {
         currentBuild.result = 'FAILURE'
         echo "Error: An error occurred while checking the file existence - ${e.getMessage()}"
@@ -18,5 +22,3 @@ def call(String fileName) {
 def isFileExists(fileName) {
     return fileExists(fileName)
 }
-
-
