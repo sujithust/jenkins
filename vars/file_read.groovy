fea4c240d44fbc92d43efdcd68c3fe7c8cdcec62
sujithust <147375608+sujithust@users.noreply.github.com>
2023-10-11 17:08:30 +0530

    Update file_read.groovy

--- vars/file_read.groovy
+++ vars/file_read.groovy
@@ -6,3 +6,23 @@ def readSampleFunction(String fileName) {
     def fileContent = readFile(file: fileName).trim()
     return fileContent
 }
+
+
+
+// @Library('sujith') _
+
+// pipeline {
+//     agent any
+
+//     stages {
+//         stage('Execute Linux Script from Shared Library') {
+//             steps {
+//                 script {
+//                     def content = file_read('/var/lib/jenkins/workspace/sample/file.txt')
+//                     echo "File Content: ${content}"
+//                 }
+//             }
+//         }
+//     }
+// }
+
